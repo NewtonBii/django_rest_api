2fd6f665f0120e8452561e64e290a81d5530b9c5
Newton Bii <biinewton382@gmail.com>
2018-03-21 13:31:07 +0300

    Make all imports in the views module.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import UserInformation
+from .serializer import UserInformationSerializer 
 # Create your views here.
